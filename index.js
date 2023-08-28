38a89cc5693d8085a47221121b7cc0c1a45d7b79
dumbeldore2 <60613998+dumbeldore2@users.noreply.github.com>
2023-08-28 18:42:55 +0200

    asse getoont int roos en werkt momenteel

--- index.js
+++ index.js
@@ -1,21 +1,51 @@
 // Initialize and add the map
 let map;
+let featureLayer;
 
 async function initMap() {
-  // The location of Uluru
-  const position = { lat: 50.919632, lng: 4.186243};
+  // The location of center
+  const position = {
+    lat: 50.919632,
+    lng: 4.186243
+  };
   // Request needed libraries.
   //@ts-ignore
-  const { Map } = await google.maps.importLibrary("maps");
-  //const { AdvancedMarkerElement } = await google.maps.importLibrary("marker");
+  const {Map} = await google.maps.importLibrary("maps");
+  const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
 
-  // The map, centered at Uluru
+  // The map, centered at asse
   map = new Map(document.getElementById("map"), {
     zoom: 14,
     center: position,
-    mapId: "DEMO_MAP_ID",
-  });
+    //mapid must be created in google maps devops 
+    //must also be vector
+    mapId: "f784cacb8ed0ec4c",
 
+  });
+  
+  //@ts-ignore
+  featureLayer = map.getFeatureLayer("LOCALITY");
+
+  // Define a style with purple fill and border.
+  //@ts-ignore
+  const featureStyleOptions = {
+    strokeColor: "#810FCB",
+    strokeOpacity: 1.0,
+    strokeWeight: 3.0,
+    fillColor: "#810FCB",
+    fillOpacity: 0.5,
+  };
+
+  // Apply the style to a single boundary.
+  //@ts-ignore
+  featureLayer.style = (options) => {
+    if (options.feature.placeId == "ChIJcbtjQ5G_w0cR7hXie1KSXWw") {
+      // Hana, HI
+      return featureStyleOptions;
+    }
+  };
+
+  
   // The marker, positioned at Uluru
   const marker = new AdvancedMarkerElement({
     map: map,
